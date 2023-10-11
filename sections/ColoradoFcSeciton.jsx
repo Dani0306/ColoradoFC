b65a50ecb5b50dd23640ccd36b7550ef8c80586f
Daniel Colorado <danicolorado0306@gmail.com>
2023-10-10 19:59:49 -0500

    third commit

--- sections/ColoradoFcSeciton.jsx
+++ sections/ColoradoFcSeciton.jsx
@@ -15,11 +15,11 @@ const ColoradoFcSeciton = () => {
             viewport={{ once: false, amount: 0.25 }}
             className={`${styles.innerWidth} mx-auto relative flex flex-col items-center justify-center`}
         >
-            <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} text-center max-w-[90%] mt-[50px]`}>
+            <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} text-center max-w-[90%] md:mt-[50px]`}>
                 Transformando sueños en logros.
             </motion.h1>
             <motion.div variants={textVariant(1.5)} className='w-max h-max'>
-                <img src="logo.png" className='w-[400px] h-[600px] object-cover' alt="logo" />
+                <img src="logo.png" className='w-[350px] h-[550px] object-cover' alt="logo" />
             </motion.div>
         </motion.div>
     </section>
