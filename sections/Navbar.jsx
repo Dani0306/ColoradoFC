d701efb39265347c042350412dcc5a86b87545ea
Daniel Colorado <danicolorado0306@gmail.com>
2023-10-13 17:01:26 -0500

    tweentyoneth-commit

--- sections/Navbar.jsx
+++ sections/Navbar.jsx
@@ -47,62 +47,15 @@ const Navbar = () => {
               <a href="#inscripcion">Inscripción</a>
           </li>
           <li className={`w-full h-[50px] flex items-center justify-start text-white font-normal ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.7s]`}>
-              <a href="#redes">Nuestras Redes</a>
-          </li>
-          <li className={`w-full h-[50px] flex items-center justify-start text-white font-normal ${showNavbar ? "appear" : "disappear"} transition-all duration-[2s]`}>
               Horarios De Entrenamiento
           </li>
-          <li className={`w-full h-[50px] flex items-center justify-start text-white font-normal ${showNavbar ? "appear" : "disappear"} transition-all duration-[2.3s]`}>
-              
+          <li className={`w-full h-[50px] flex items-center justify-start text-white font-normal ${showNavbar ? "appear" : "disappear"} transition-all duration-[2s]`}>
+              <a href="#premios">Estrellas Destacadas</a>
           </li>
-          
-           {/* <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-500 text-xl text-white my-2 lg:hidden hover:cursor-pointer`}>
-                <Link href="/dashboard">
-                  Dashboard
-                </Link>
-            </li>
-              <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[.7s] text-xl text-white my-2 lg:hidden hover:cursor-pointer`}>
-                <Link href="/bets">
-                  My Bets
-                </Link>
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[.9s] text-xl text-white my-2 lg:hidden hover:cursor-pointer`}>
-                New Bet
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.1s] text-xl text-white my-2 lg:hidden hover:cursor-pointer`}>
-                <Link href="/statistics">
-                  Statistics
-                </Link>
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.3s] text-xl text-white my-2 lg:hidden hover:cursor-pointer`}>
-                <Link href="/bank-management">
-                  Bank management
-                </Link>
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.5s] text-xl lg:duration-[.7s] text-white my-2 hover:cursor-pointer`}>
-                Settings
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.7s] text-xl lg:duration-[.9s] text-white my-2 hover:cursor-pointer`}>
-                Login
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[1.9s] text-xl lg:duration-[1.1s] text-white my-2 hover:cursor-pointer`}>
-                <Link href="/help">
-                  Help
-                </Link>
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[2.1s] text-xl lg:duration-[1.3s] text-white my-2 hover:cursor-pointer`}>
-                <Link href="/about">
-                  About Us
-                </Link>
-            </li>
-            <li className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[2.3s] text-xl lg:duration-[1.5s] text-white my-2 hover:cursor-pointer`}>
-                <Link href="/contact">
-                  Contact us
-                </Link>
-            </li>
-            <li  className={` ${showNavbar ? "appear" : "disappear"} transition-all duration-[2.5s] text-xl lg:duration-[1.7s] text-white my-2 hover:cursor-pointer`}>
-                Logout
-            </li> */}
+          <li className={`w-full h-[50px] flex items-center justify-start text-white font-normal ${showNavbar ? "appear" : "disappear"} transition-all duration-[2.3s]`}>
+              <a href="#redes">Nuestras Redes</a>
+          </li>
+    
           </ul>
     </motion.nav>
   )
