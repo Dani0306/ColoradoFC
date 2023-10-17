4c2093c0a187fb614584d7b5c9dea36699c879fc
Daniel Colorado <danicolorado0306@gmail.com>
2023-10-17 16:39:03 -0500

    controls of the video

--- sections/Awards.jsx
+++ sections/Awards.jsx
@@ -7,7 +7,7 @@ import { contentAwards } from '../utils/constants';
 import { TitleText, TypingText } from '../components/CustomTexts';
 import AwardComponent from "../components/AwardComponent"
 import { staggerContainer, fadeIn } from '../utils/motion';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/youtube';
 
 const Awards = () => (
   <section id="reconocimientos" className={`${styles.paddings} relative z-10 overflow-hidden`}>
@@ -40,7 +40,7 @@ const Awards = () => (
           alt="get-started"
           className="w-[90%] h-[90%] object-contain"
           >
-            <ReactPlayer url={"https://www.youtube.com/watch?v=s-1yHi-dcSw"} width='100%' height='100%'/>
+            <ReactPlayer url={"https://www.youtube.com/watch?v=s-1yHi-dcSw"} width='100%' height='100%' controls={true}/>
         </div>
         
       </motion.div>
