8eef3e6fff28c715e5d0fd533fc75ac5175515fe
Daniel Colorado <danicolorado0306@gmail.com>
2023-10-20 20:45:45 -0500

    .

--- components/Category.jsx
+++ components/Category.jsx
@@ -9,7 +9,7 @@ import SlidePrevButton from './SlidePrevButton'
 const Category = ({ items, activeIndex }) => {
   return (
         <div className='w-full h-full flex items-center justify-center'>
-            <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.25 }} className="relative bg-[url('/formacion.jpeg')] bg-cover bg-center w-full h-[350px] lg:w-[900px] lg:h-[500px] items-center justify-center flex flex-col rounded-xl mt-[80px]">
+            <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.25 }} className="relative bg-[url('/formacion.jpeg')] bg-cover bg-center w-full h-[350px] lg:w-[900px] lg:h-[500px] items-center justify-start lg:justify-center flex flex-col rounded-xl mt-[80px] pt-[50px] lg:pt-auto">
               <SlidePrevButton activeIndex={activeIndex}/>
               <SlideNextButton activeIndex={activeIndex}/>
           <div className='absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.75)]'/>
